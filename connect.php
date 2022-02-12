9e275000492df700c687a94c6c8d83f78803c6da
Remku <nganaremba@gmail.com>
2022-02-12 12:11:20 +0530

    Updation for localhost

--- connect.php
+++ connect.php
@@ -1,9 +1,17 @@
-<?php
-    $host = 'sql105.epizy.com';
-    $user = 'epiz_30782311';
-    $pass = 'hM3YKIwoZoeJZG';
-    $db_name = 'epiz_30782311_chat';
-
-    $con = mysqli_connect($host,$user,$pass,$db_name) or die('Error connecting to server');
-
+<?php
+
+    $host = 'localhost';
+
+    $user = 'root';
+
+    $pass = '';
+
+    $db_name = 'chat';
+
+
+
+    $con = mysqli_connect($host,$user,$pass,$db_name) or die('Error connecting to server');
+
+
+
 ?>
